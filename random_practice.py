7f1f89aef3c188f829551fc3a1fe073958fa1fc5
Hassen <smilex396>
2026-06-05 10:03:38 +0300

    try matplotlib

--- random_practice.py
+++ random_practice.py
@@ -1,19 +1,29 @@
-from typing import List
+def longestPalindrome( s: str) -> str:
+    n = len(s)
+    s = 'and'
+    start = 0
+    max_len = 1
+    for i in range(1, n):
+        if s[i] == s[i - 1]:
+            left = i - 1
+            right = i
+            while left >= 0 and right < n:
+                if s[left] == s[right] and max_len < right - left + 1:
+                    start = left
+                    max_len = right - left + 1
+                elif s[left] != s[right]:
+                    break
+                left -= 1
+                right += 1
+    return ''
+
+# print(longestPalindrome('cbbd'))
 
 
-def solveQueries(nums: List[int], queries: List[int]) -> List[int]:
-    min_distance = float('inf')
-    answer = []
-    for i in range(len(queries)):
-        target_index = queries[i]
-        for j in range(len(nums)):
-            if j != target_index and nums[j] == nums[target_index]:
-                min_distance = min(min_distance, abs(target_index - j), (len(nums) - target_index + j) if target_index > j else abs(len(nums) - j -target_index))
-        if min_distance == float('inf'):
-            answer.append(-1)
-            continue
-        answer.append(min_distance)
-        min_distance = float('inf')
-    return answer
+def test():
+    nums = []
+    nums.insert(1, 1)
+    nums.append([4,5])
+    print([2,3] + nums)
 
-ans = solveQueries([6,12,17,9,16,7,6], [5,6,0,4])
+test()
